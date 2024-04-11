554eaf823a45ef10a6243cd5526101993aa2bed9
shtykhnoia <shtykhno.ilya@mail.ru>
2024-04-11 19:25:27 +0300

    Переместил модели в другую папку и создал таблицу User

---
+++ backend/model/users.py
@@ -0,0 +1,18 @@
+import sqlalchemy
+from sqlalchemy import orm, Column, VARCHAR
+from datetime import datetime
+from .db_session import Base
+
+
+class User(Base):
+    __tablename__ = 'User'
+
+    username = Column(VARCHAR, primary_key=True, nullable=False)
+    password = Column(VARCHAR, nullable=False)
+
+    profile = orm.relationship('Profile', back_populates='user')
+
+    def __repr__(self):
+        return f'<Username {self.username}>'
+
+
