d8c3efef0395edf03808367f13b7ef11ef1de4f6
Юсупов Артем <artem597597@mail.ru>
2024-04-19 21:20:56 +0300

    Charts for dashboard

--- frontend/src/pages/dashboard/home.jsx
+++ frontend/src/pages/dashboard/home.jsx
@@ -27,7 +27,8 @@ import {
 } from "@/data";
 import { CheckCircleIcon, ClockIcon } from "@heroicons/react/24/solid";
 import {useMaterialTailwindController} from "../../context/index.jsx";
-
+import DoughnutChartSelection from '../../modules/dashboard/components/DoughnutChartSelection'
+import LineChart from '../../modules/dashboard/components/LineChart'
 export function Home() {
 
   const [controller, dispatch] = useMaterialTailwindController();
@@ -54,21 +55,9 @@ export function Home() {
         ))}
       </div>
       <div className="mb-6 grid grid-cols-1 gap-y-12 gap-x-6 md:grid-cols-2 xl:grid-cols-3">
-        {statisticsChartsData.map((props) => (
-          <StatisticsChart
-            key={props.title}
-            {...props}
-            footer={
-              <Typography
-                variant="small"
-                className="flex items-center font-normal text-blue-gray-600"
-              >
-                <ClockIcon strokeWidth={2} className="h-4 w-4 text-blue-gray-400" />
-                &nbsp;{props.footer}
-              </Typography>
-            }
-          />
-        ))}
+        <DoughnutChartSelection/>
+        <LineChart/>
+        <LineChart/>
       </div>
       <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-3">
         <Card className="overflow-hidden xl:col-span-2 border border-blue-gray-100 shadow-sm">
