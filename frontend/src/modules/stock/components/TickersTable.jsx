563331fb614e9e0c7c24cd4c31ac9b4d33468058
Yusupov Artem <artem597597@mail.ru>
2024-04-20 12:02:44 +0300

    Updating data in time

--- frontend/src/modules/stock/components/TickersTable.jsx
+++ frontend/src/modules/stock/components/TickersTable.jsx
@@ -31,53 +31,6 @@ import appStore from "../../../stores/AppStore.jsx";
 
 const TABLE_HEAD = ["Название", "Цена", "За день", "За год", "График"];
 
-const TABLE_ROWS = [
-    {
-        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-3.jpg",
-        name: "John Michael",
-        email: "john@creative-tim.com",
-        job: "Manager",
-        org: "Organization",
-        online: true,
-        date: "23/04/18",
-    },
-    {
-        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-2.jpg",
-        name: "Alexa Liras",
-        email: "alexa@creative-tim.com",
-        job: "Programator",
-        org: "Developer",
-        online: false,
-        date: "23/04/18",
-    },
-    {
-        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-1.jpg",
-        name: "Laurent Perrier",
-        email: "laurent@creative-tim.com",
-        job: "Executive",
-        org: "Projects",
-        online: false,
-        date: "19/09/17",
-    },
-    {
-        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-4.jpg",
-        name: "Michael Levi",
-        email: "michael@creative-tim.com",
-        job: "Programator",
-        org: "Developer",
-        online: true,
-        date: "24/12/08",
-    },
-    {
-        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-5.jpg",
-        name: "Richard Gran",
-        email: "richard@creative-tim.com",
-        job: "Manager",
-        org: "Executive",
-        online: false,
-        date: "04/10/21",
-    },
-];
 
 export const TickersTable =  observer(({category, tickers}) => {
 
@@ -226,7 +179,7 @@ export const TickersTable =  observer(({category, tickers}) => {
                     {
                         stockStore.isLoading || !tickers ? <LoadingSpinner isInTable={true}/> : tickers.map(
                         (el, index) => {
-                            const isLast = index === TABLE_ROWS.length - 1;
+                            const isLast = index === 4;
                             const classes = isLast
                                 ? "p-4"
                                 : "p-4 border-b border-blue-gray-50";
@@ -241,6 +194,16 @@ export const TickersTable =  observer(({category, tickers}) => {
                                                 <div>
                                                     <Typography variant="h6" color={'black'}>{el.name}</Typography>
                                                     <Typography variant="small" color="gray" className="font-normal">{el.ticker}</Typography>
+                                                </div>
+                                                <div>
+                                
+                                                    <IconButton 
+                                                         ripple={true} 
+                                                         variant="text"
+                                                         color="blue"
+                                                         title="В избранное">
+                                                            <i className="far fa-star"/>
+                                                    </IconButton>
                                                 </div>
                                             </div>
                                     </td>
--- frontend/src/modules/stock/view/TickerData.tsx
+++ frontend/src/modules/stock/view/TickerData.tsx
@@ -37,6 +37,17 @@ export const TickerData = (() => {
 
     useEffect(() => {
         setIsLoading(true)
+     
+    
+        fetchTickerData()
+
+        const interval = setInterval(fetchTickerData, 5000); // Вызов функции каждые 30 секунд
+
+        return () => clearInterval(interval); // Очистка интервала при размонтировании компонента
+    
+    }, []);
+
+    const fetchTickerData = () => {
         const category = window.location.pathname.slice(window.location.pathname.indexOf('/', 2) + 1, window.location.pathname.lastIndexOf('/'))
         const tickerName = window.location.pathname.slice(window.location.pathname.lastIndexOf('/') + 1)
 
@@ -47,15 +58,15 @@ export const TickerData = (() => {
         }
 
         stockStore.getDataByTickerName(params)
-            .then((response) => response.json())
-            .then((json) => {
-                setLastPrice(JSON.parse(json[0]).prices[0])
-                setCandles(JSON.parse(json[1]).candles)
-                setGrowthPerTimeFrame(JSON.parse(json[1]).growPerYearPercent)
-                setTickerData(json[2])
-                setIsLoading(false)
-            })
-    }, []);
+        .then((response) => response.json())
+        .then((json) => {
+            setLastPrice(JSON.parse(json[0]).prices[0])
+            setCandles(JSON.parse(json[1]).candles)
+            setGrowthPerTimeFrame(JSON.parse(json[1]).growPerYearPercent)
+            setTickerData(json[2])
+            setIsLoading(false)
+        })
+    }
 
     const onChartColorChange = (color) => {
         setChartColor(color.hex)
@@ -66,38 +77,52 @@ export const TickerData = (() => {
             {
                isLoading ? <LoadingSpinner isInTable={false}/> :  <div className="">
                    <Card className={"bg-gradient-to-br from-gray-500 to-gray-900 rounded-lg "}>
-                       <CardBody className="flex justify-evenly">
-                           <div className="flex flex-row gap-6">
-                               <Avatar
-                                   src={`https://invest-brands.cdn-tinkoff.ru/${tickerData.instrument.brand.logo_name.replace('.png', '')}x640.png`}
-                                   alt={'avatar'} size="xl"/>
-                               <div>
-                                   <Typography color={textColor} variant={"h4"} className={"font-medium"}>
-                                       {tickerData.instrument.name}
-                                   </Typography>
-                                   <Typography color={textColor} variant={"small"} className={"font-medium"}>
-                                       {tickerData.instrument.ticker}
-                                   </Typography>
-                               </div>
-                           </div>
-                           <div className="flex flex-row gap-4 ">
-                               <div className="flex flex-col">
-                                   <Typography color={textColor} variant={"h5"} className={"font-light"}>
-                                       Доходность за год
-                                   </Typography>
-                                   <Typography color={textColor} variant={"h3"} className={"font-bold"}>
-                                       {growthPerTimeFrame.toFixed(2) } %
-                                   </Typography>
-                               </div>
-                               <div className="flex flex-col">
-                                   <Typography color={textColor} variant={"h5"} className={"font-light"}>
-                                       Cтоимость
-                                   </Typography>
-                                   <Typography color={textColor} variant={"h3"} className={"font-bold"}>
-                                        {lastPrice + getSymbolFromCurrency(tickerData.instrument.currency)}
-                                   </Typography>
-                               </div>
-                           </div>
+                       <CardBody className='grid grid-cols-[1fr_2fr_1fr]'>
+                       <div></div>
+                        <div className='flex justify-evenly'>
+                            <div className="flex flex-row gap-6">
+                                <Avatar
+                                    src={`https://invest-brands.cdn-tinkoff.ru/${tickerData.instrument.brand.logo_name.replace('.png', '')}x640.png`}
+                                    alt={'avatar'} size="xl"/>
+                                <div>
+                                    <Typography color={textColor} variant={"h4"} className={"font-medium"}>
+                                        {tickerData.instrument.name}
+                                    </Typography>
+                                    <Typography color={textColor} variant={"small"} className={"font-medium"}>
+                                        {tickerData.instrument.ticker}
+                                    </Typography>
+                                </div>
+                            
+                            </div>
+                            <div className="flex flex-row gap-4 ">
+                                <div className="flex flex-col">
+                                    <Typography color={textColor} variant={"h5"} className={"font-light"}>
+                                        Доходность за год
+                                    </Typography>
+                                    <Typography color={textColor} variant={"h3"} className={"font-bold"}>
+                                        {growthPerTimeFrame.toFixed(2) } %
+                                    </Typography>
+                                </div>
+                                <div className="flex flex-col">
+                                    <Typography color={textColor} variant={"h5"} className={"font-light"}>
+                                        Cтоимость
+                                    </Typography>
+                                    <Typography color={textColor} variant={"h3"} className={"animate-pulse font-bold"}>
+                                            {lastPrice + getSymbolFromCurrency(tickerData.instrument.currency)}
+                                    </Typography>
+                                </div>
+                            </div>
+                        </div> 
+                        <div className="flex justify-end">
+                                <IconButton 
+                                size='lg'
+                                     ripple={true} 
+                                     variant="text"
+                                     color="yellow"
+                                     title="В избранное">
+                                        <i className="far fa-star"/>
+                                </IconButton>
+                        </div>
                        </CardBody>
                    </Card>
                    <div className="mt-5 flex justify-evenly">
